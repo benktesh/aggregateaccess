dc0763821cd1673f6493c2d7a54612540d932bbf
benktesh <benktesh@yahoo.com>
2019-06-10 21:09:29 -0500

    Add project files.

---
+++ Aggregate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aggregate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
